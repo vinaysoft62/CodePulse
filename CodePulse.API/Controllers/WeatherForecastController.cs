44d8158e9a1140dd31c08e48dfe13364ae964fa4
vinaysoft62 <vinaysoft62@gmail.com>
2025-01-30 12:24:18 +0530

    WeatherForecast changes

--- CodePulse.API/Controllers/WeatherForecastController.cs
+++ CodePulse.API/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace CodePulse.API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Very Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
